efcce4524a1da71218c66439cea7d6be51c5a1e7
Togene <jhonnywhiplash@hotmail.com.au>
2017-10-01 15:58:47 +1000

    Added Genres, Moved Model to Migration

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public String Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public byte NumberInStock { get; set; }
     }
 
     // /movies/random
